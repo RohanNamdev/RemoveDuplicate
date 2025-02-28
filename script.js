9e51b172ef6e5f1a0603b847535611bc7964755b
RohanNamdev <rnamdev715@gmail.com>
2025-02-28 18:43:23 +0530

    Add my code

---
+++ script.js
@@ -0,0 +1,21 @@
+const eggScrambleRecipe = [
+    "🥓 bacon",
+    "🥓 bacon",
+    "🍳 eggs",
+    "🫑 green peppers",
+    "🧀 cheese",
+    "🌶️ hot sauce",
+    "🥓 bacon",
+    "🥦 broccoli",
+    "🧀 cheese",
+    "🥦 broccoli",
+    "🌶️ hot sauce"
+]
+
+function removeDupesFromArray(arr) {
+    return [...new Set(arr)]
+
+
+}
+
+console.log(removeDupesFromArray(eggScrambleRecipe));
